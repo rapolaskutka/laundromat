bf913ba67a011434b498c4e207a3e18c92733c06
rapolas.kutka <rapolas.kutka@nfq.lt>
2023-11-26 22:14:59 +0200

    History order

--- src/Entity/History.php
+++ src/Entity/History.php
@@ -11,7 +11,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: HistoryRepository::class)]
-#[ApiResource]
+#[ApiResource(order: ['id' => 'DESC'])]
 #[ApiResource(
     uriTemplate: '/dorms/{dormId}/machines/{machineId}/histories/{id}',
     operations: [ new Get() ],
--- src/Repository/HistoryRepository.php
+++ src/Repository/HistoryRepository.php
@@ -20,29 +20,4 @@ class HistoryRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, History::class);
     }
-
-//    /**
-//     * @return History[] Returns an array of History objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('h')
-//            ->andWhere('h.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('h.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?History
-//    {
-//        return $this->createQueryBuilder('h')
-//            ->andWhere('h.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
